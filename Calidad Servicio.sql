65baa9b07c82bb7dcae95f89080b5e1a0ad50732
ABZblack <109823782+ABZblack@users.noreply.github.com>
2022-08-12 14:20:52 -0500

    Update Calidad Servicio.sql

--- Calidad Servicio.sql
+++ Calidad Servicio.sql
@@ -1,4 +1,4 @@
--- *** CONSULTA CONSUMOS ****
+-- *** CONSULTA CONSUMOS DE LA LÍNEA  ****
 
 
 SELECT (downlink/1024)/1024 AS Trafico_Bajada, (uplink/1024)/1024 AS Trafico_subida, calling_party_imsi, calling_party_number, enodeb, cell_id, event_hour,
@@ -18,3 +18,41 @@ where event_month =202207
 and enodeb in ( 250118 )
 --and site_id in (910200)
 and event_day = 20220720
+
+
+
+-- *** CONSUMOS DE UN SITIO, EN AFFIRMED Y NOKIA ***
+
+
+SELECT * FROM ( SELECT event_hour, enodeb, cell_id, core, enodeb_group, SUM(ctd_users) AS ctd_users, SUM(consumo_GB) AS consumo_GB
+FROM (
+  
+SELECT event_hour, enodeb, cell_id,
+zeroifnull(enodeb) + quotient(zeroifnull(cell_id),10)/10 as 'enodeb_group',
+'nokia'AS core, COUNT(DISTINCT calling_party_imsi) AS ctd_users,
+SUM((accounting_input_octets+accounting_output_octets)/1024/1024/1024) AS consumo_GB
+FROM sgw.sgw_cdrs
+WHERE event_month IN(${mes})
+AND event_hour BETWEEN ${Hora_inicio} AND ${Hora_fin}
+AND enodeb IN (${enodeb})
+GROUP BY 1,2,3,4,5
+UNION ALL
+SELECT event_hour, enodeb, cell_id,
+zeroifnull(enodeb) + quotient(zeroifnull(cell_id),10)/10 as 'enodeb_group',
+'affirmed'AS core, COUNT(DISTINCT calling_party_imsi) AS ctd_users,
+SUM((accounting_input_octets+accounting_output_octets)/1024/1024/1024) AS consumo_GB
+FROM sgw.sgw_cdrs_affirmed
+WHERE event_month IN(${mes})
+AND event_hour BETWEEN ${Hora_inicio} AND ${Hora_fin}
+AND enodeb IN (${enodeb})
+GROUP BY 1,2,3,4,5
+) AS sgw_cdrd
+GROUP BY 1,2,3,4,5
+) AS scdrs
+LEFT OUTER JOIN
+(
+SELECT DISTINCT site_id, zeroifnull(enodeb) + quotient(zeroifnull(cell_id),10)/10 as 'enodeb_group', site_name,
+site_latitude, site_longitude, state_name, region, municipality_name, site_type, tx_output
+FROM catalog.catalog_cell
+) AS cat
+ON scdrs.enodeb_group = cat.enodeb_group;
