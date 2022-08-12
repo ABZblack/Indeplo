cda39846ecbe246180dcfa1707ac5cee96ae0d0c
ABZblack <109823782+ABZblack@users.noreply.github.com>
2022-08-12 14:26:42 -0500

    Update Calidad Servicio.sql

--- Calidad Servicio.sql
+++ Calidad Servicio.sql
@@ -14,10 +14,10 @@ ORDER BY event_hour DESC;
 
 select site_name, service_state, municipality, state, volume_dl, ue_dl_throughput, sector, cell_sector, site_id, enodeb, latitude, longitude
 from service.oss_sectors_busy_hour
-where event_month =202207
-and enodeb in ( 250118 )
+where event_month = ${evetnt_month} 
+and enodeb in ( ${sitio} )
 --and site_id in (910200)
-and event_day = 20220720
+and event_day = ${fecha}
 
 
 
