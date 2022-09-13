2ae9efb3c5122cb43a33b93e23ff06a1222b701a
ABZblack <109823782+ABZblack@users.noreply.github.com>
2022-09-13 18:47:41 -0500

    Create consumos.sql

---
+++ HBB/consumos.sql
@@ -0,0 +1,10 @@
+-- *** CONSULTA CONSUMOS DE LA LÍNEA  ****
+
+
+SELECT (downlink/1024)/1024 AS Trafico_Bajada, (uplink/1024)/1024 AS Trafico_subida, calling_party_imsi, calling_party_number, enodeb, cell_id, event_hour,
+concat(cast(enodeb as string), '.', cast(cell_id as string)) as SITIO
+--fROM sgw.sgw_cdrs_aggregates --Nokia
+frOM sgw.sgw_cdrs_affirmed_aggregates --Affirmed
+WHERE calling_party_imsi in(${IMSI})
+AND event_hour between ${fecha_incio} and ${fecha_fin}
+ORDER BY event_hour DESC;
