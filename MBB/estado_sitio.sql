db68e97eb137020cfd782da69ef5d61c51cb2c71
ABZblack <109823782+ABZblack@users.noreply.github.com>
2022-09-30 12:13:28 -0500

    Create estado_sitio.sql

---
+++ MBB/estado_sitio.sql
@@ -0,0 +1,10 @@
+select concat(cast(enodeb as string), '.', cast(cell_id as string)) as SITIO,
+round(sum(accounting_input_octets + accounting_output_octets)/1024/1024,2) as Trafico,
+count(distinct calling_party_imsi) as Usuarios
+--from sgw.sgw_cdrs --nokia
+FROM sgw.sgw_cdrs_affirmed --affirmed
+where enodeb in (${enodB})
+and event_month = ${mes}
+and event_hour between ${Fecha_inicio} and ${Fecha_fin}
+group by 1
+;
