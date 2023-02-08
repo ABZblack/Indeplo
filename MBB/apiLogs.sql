f363998ba1d0d7f2110c38cbd1b1e0dfc383d257
ABZblack <109823782+ABZblack@users.noreply.github.com>
2023-02-08 15:32:12 -0600

    Create apiLogs.sql

---
+++ MBB/apiLogs.sql
@@ -0,0 +1,9 @@
+--api ilogs
+
+SELECT *--operation, log_timestamp,order_id, subscriber_id, apiparent
+FROM apigee.apigee_logs
+WHERE 
+event_hour between 2022080900 and 2023042500
+and be_id IN (142)
+and subscriber_id in ( 5581613828)
+ORDER BY event_hour DESC;
