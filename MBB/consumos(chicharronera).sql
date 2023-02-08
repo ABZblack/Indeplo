2db18ea80ae316c6d529d4a1b248c9f700b9ffde
ABZblack <109823782+ABZblack@users.noreply.github.com>
2023-02-08 15:30:48 -0600

    Create consumos(chicharronera).sql

---
+++ MBB/consumos(chicharronera).sql
@@ -0,0 +1,11 @@
+SELECT calling_party_imsi, calling_party_number, cust_local_start_date,
+cast(rate_usage AS REAL)/1024/1024 AS
+Consumo_MB, mcc_tai, mnc_tai,
+calling_roam_info, enodeb, cell_id
+FROM bss.cbs_cdrs_data 
+WHERE 
+billing_month = ${mes}
+and event_hour BETWEEN ${Fecha_Inicio} and ${Fecha_Fin}
+and be_id = ${BE_ID}
+and calling_party_number = ${DN}
+ORDER BY event_hour DESC
