57005577d9fff32eb44378e621ba32adb864bb60
ABZblack <109823782+ABZblack@users.noreply.github.com>
2023-02-16 12:37:44 -0600

    Update apiLogs.sql

--- MBB/apiLogs.sql
+++ MBB/apiLogs.sql
@@ -5,5 +5,5 @@ FROM apigee.apigee_logs
 WHERE 
 event_hour between 2022080900 and 2023042500
 and be_id IN (142)
-and subscriber_id in ( 5581613828)
+and subscriber_id in ( 5581613828) --DN a 10 digitos
 ORDER BY event_hour DESC;
