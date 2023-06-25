c4de539a3246701d00d5255779a297df28dedf0a
ABZblack <109823782+ABZblack@users.noreply.github.com>
2023-06-25 12:33:58 -0600

    Update portabilidad.sql

--- MBB/portabilidad.sql
+++ MBB/portabilidad.sql
@@ -1,11 +1,5 @@
 SELECT *
 FROM bss.portability_logs
 WHERE
-msisdn_ported IN (7711977976) --buscar por úmero portado
-;
-
-SELECT *
-FROM bss.portability_logs
-WHERE
-or msisdn_backup in (7714290717) --buscar por número transitorio
-;
+msisdn_ported IN (7226598206)or msisdn_backup in (7226598206)
+ORDER BY load_processing_timestamp desc;
