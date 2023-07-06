f519abc0682035e8701af3504c2fcc4fcff0f912
ABZblack <109823782+ABZblack@users.noreply.github.com>
2023-07-06 11:08:44 -0600

    Delete bonos.sql

--- bonos.sql
+++
@@ -1,5 +0,0 @@
-SELECT nombre_promocion, offer_id_adquirido, offer_id_promocion, fecha_inicio_vigencia_promocion,fecha_fin_vigencia_promocion,
-load_processing_timestamp
-FROM service.catalogo_promociones
-where be_id = ${BE_ID}
-;
