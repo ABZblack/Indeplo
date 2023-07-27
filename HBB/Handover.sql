932e295384792adfe09cc851b8e81d5d19c3a145
ABZblack <109823782+ABZblack@users.noreply.github.com>
2023-07-27 17:51:00 -0600

    Update Handover.sql

--- HBB/Handover.sql
+++ HBB/Handover.sql
@@ -1,9 +1,17 @@
 -- ===================================================================================== QUERY HANDOVER ===================================
 
-select distinct source_site_id, source_sector, target_site_id, target_sector
-from oss.catalog_handover_huawei_sites
-where source_site_id = ${eNodB}
-union all
-select distinct source_site_id, source_sector, target_site_id, target_sector
+SELECT DISTINCT source_enodeb, source_site_id, source_sector, target_enodeb, target_site_id, target_sector, carrier
+FROM
+(
+select distinct source_enodeb, source_site_id, source_sector, target_enodeb, target_site_id, target_sector, 'nokia' AS carrier
 from oss.catalog_handover_nokia_sites
-where source_site_id = ${eNodB}
+where source_site_id = ${BS} -- Best Server
+AND target_site_id = ${TF} -- Sitio que suspende
+
+UNION ALL
+
+select distinct source_enodeb, source_site_id, source_sector, target_enodeb, target_site_id, target_sector, 'huawei' AS carrier
+from oss.catalog_handover_huawei_sites
+where source_site_id = ${BS} -- Best Server
+AND target_site_id = ${TF}  -- Sitio que suspende
+) AS a;
