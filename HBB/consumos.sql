78482d8471b82dfd6d95591006cbb6a145e48978
ABZblack <109823782+ABZblack@users.noreply.github.com>
2023-08-15 17:23:38 -0600

    Update consumos.sql

--- HBB/consumos.sql
+++ HBB/consumos.sql
@@ -2,7 +2,7 @@
 
 
 SELECT (downlink/1024)/1024 AS Trafico_Bajada, (uplink/1024)/1024 AS Trafico_subida, calling_party_imsi, calling_party_number, enodeb, cell_id, event_hour,
-concat(cast(enodeb as string), '.', cast(cell_id as string)) as SITIO
+concat(cast(enodeb as string), '.', cast(cell_id as string)) as SITIO, imei
 --fROM sgw.sgw_cdrs_aggregates --Nokia
 frOM sgw.sgw_cdrs_affirmed_aggregates --Affirmed
 WHERE calling_party_imsi in(${IMSI})
