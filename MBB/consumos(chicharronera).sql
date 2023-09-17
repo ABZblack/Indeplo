f92fb270a0ec700e8fc70a7674cf7f6ecbf4be38
ABZblack <109823782+ABZblack@users.noreply.github.com>
2023-09-17 15:02:40 -0600

    Update consumos(chicharronera).sql

--- MBB/consumos(chicharronera).sql
+++ MBB/consumos(chicharronera).sql
@@ -1,22 +1,24 @@
-SELECT enodeb,
-       cell_id,
-       called_station_id,
-       round(sum(accounting_input_octets + accounting_output_octets)/1024/1024,2) AS Trafico,
-       calling_party_imsi,
-       calling_party_number,
-       event_timestamp,
-       event_hour,
-       imei,
-       mcc_tai,
-       mnc_tai,
-       qos_class_identifier,
-       qos_class_identifier_value
-FROM sgw.sgw_cdrs
-WHERE
-calling_party_imsi IN (334140143623531)  --IMSI
---calling_party_number IN (528137147550) -- DN 12 digitos
-and event_month = 202309
-AND event_hour BETWEEN 2023091200 and 2023091323
 
-GROUP BY 1,2,3,5,6,7,8,9,10,11,12,13
+SELECT  event_hour,
+        enodeb,
+        cell_id,
+        calling_party_number,
+        calling_party_imsi,
+        sum(cast(rate_usage as real)/1024/1024) as consumo_MB,
+        rating_group,
+        fu_charging_offer_id,
+        calling_roam_info,
+        free_unit_type_1,
+        apn
+FROM bss.cbs_cdrs_data
+WHERE 
+--calling_party_imsi = 334140015292897
+calling_party_number = 525534643482
+and be_id= 143
+and billing_month = 202309
+and event_hour BETWEEN 2023091400 and 2023112223
+GROUP BY 1,2,3,4,5,7,8,9,10,11
+ORDER BY event_hour DESC
+;
+
 ORDER BY event_hour DESC;
