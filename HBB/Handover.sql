ce91b48e0f20ecc8e13ecc35b963316ef5b8e985
ABZblack <109823782+ABZblack@users.noreply.github.com>
2023-11-07 16:22:51 -0600

    Update Handover.sql

--- HBB/Handover.sql
+++ HBB/Handover.sql
@@ -1,17 +1,30 @@
 -- ===================================================================================== QUERY HANDOVER ===================================
 
+-------------------------- ENODEB -------------------------
 SELECT DISTINCT source_enodeb, source_site_id, source_sector, target_enodeb, target_site_id, target_sector, carrier
 FROM
 (
 select distinct source_enodeb, source_site_id, source_sector, target_enodeb, target_site_id, target_sector, 'nokia' AS carrier
 from oss.catalog_handover_nokia_sites
-where source_site_id = ${BS} -- Best Server
-AND target_site_id = ${TF} -- Sitio que suspende
+-- Para cuando se tiene solo los nodos:
+where source_enodeb = ${BS} -- Best Server nod
+AND target_enodeb = ${TF} -- nodo que suspende
+
+--Para cuando se tiene los site_id:
+--where source_site_id = ${BS} -- Best Server site id
+--AND target_site_id = ${TF} --  site id que suspende
+
 
 UNION ALL
 
 select distinct source_enodeb, source_site_id, source_sector, target_enodeb, target_site_id, target_sector, 'huawei' AS carrier
 from oss.catalog_handover_huawei_sites
-where source_site_id = ${BS} -- Best Server
-AND target_site_id = ${TF}  -- Sitio que suspende
+--Para cuando se tiene solo los nodos:
+where source_enodeb = ${BS} -- Best Server
+AND target_enodeb = ${TF}  -- Sitio que suspende
+
+--Para cuando se tiene los site_id:
+--where source_site_id = ${BS} -- Best Server site id
+--AND target_site_id = ${TF} --  site id que suspende
 ) AS a;
+
