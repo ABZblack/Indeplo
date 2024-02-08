5043a1a1c1e11b9394b84041ea3d93dc55d0ccd0
ABZblack <109823782+ABZblack@users.noreply.github.com>
2024-02-08 16:50:54 -0600

    Create IMEI_por_DN.sql

---
+++ MBB/IMEI_por_DN.sql
@@ -0,0 +1,63 @@
+with 
+
+imeis as
+(
+    SELECT  tac,
+            category as homologado,
+            case
+                when volte = '1' then 'Si'
+                else 'no'
+            
+            end as volteCapable,
+            case
+                when band_28 = 1 then 'Si'
+                else 'no'
+            
+            end as Band_28,
+            gsma_model_name as model,
+            gsma_brand_name as brand,
+            gsma_manufacturer as manufacturer,
+            gsma_device_type as device_type,
+            sub_category as subcategory,
+            case
+                when esim = 1 then 'Si'
+                else 'no'
+            
+            end as E_SIM,
+            gsma_operating_system,
+            gsma_country_code
+        
+        from service.device_map_integrated
+),
+
+consulta_imei as
+(
+    SELECT substr(cast(imei as STRING),1, 8) as tac, imei,event_hour
+     FROM (
+            SELECT imei, event_hour
+            FROM pgw.pgw_cdrs
+            where calling_party_number in (${DN_12digitos})
+            and event_month >= CAST(from_timestamp(date_sub(now(),3),'yyyyMM') AS BIGINT)
+            AND event_hour >= CAST(from_timestamp(date_sub(now(),2),'yyyyMMddHH') AS BIGINT)
+        ) as a
+        
+    where imei is not null
+    order by event_hour desc
+    limit 1
+)
+
+SELECT  c.imei, 
+        i.homologado,
+        i.volteCapable,
+        i.Band_28, 
+        i.model,
+        i.brand,
+        i.manufacturer,
+        i.device_type,
+        i.subcategory,
+        i.e_sim,
+        i.gsma_operating_system,
+        i.gsma_country_code
+from consulta_imei as c
+left join imeis as i
+ON c.tac=i.tac
