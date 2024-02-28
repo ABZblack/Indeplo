b2d7fdc89aea5e432f446128507602fcf8281976
ABZblack <109823782+ABZblack@users.noreply.github.com>
2024-02-28 10:51:55 -0600

    Update bearers.sql

--- MBB/bearers.sql
+++ MBB/bearers.sql
@@ -1,26 +1,22 @@
 --*************Bearers*****************---
 -- sesiones de datos y IMS
 
------------------------------------------------ IMS-------
-
 SELECT enodeb,
        cell_id,
        called_station_id,
        round((accounting_input_octets + accounting_output_octets)/1024.0/1024.0,2) AS Trafico,
        calling_party_imsi,
        calling_party_number,
-       change_time,
        event_hour,
        event_timestamp,
        imei,
-       mcc_tai,
-       mnc_tai,
        qos_class_identifier,
        qos_class_identifier_value
 FROM sgw.sgw_cdrs
 WHERE
 --calling_party_imsi IN (334140169514164)  --IMSI
-calling_party_number IN (525635290990) -- DN 12 digitos
-and event_month >= 202312
-AND event_hour >= 2023120100
+calling_party_number IN (525513565501) -- DN 12 digitos
+and event_month >= 202401
+AND event_hour >= 2024022400
 ORDER BY event_hour DESC;
+
