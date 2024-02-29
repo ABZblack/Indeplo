68c8006c7588c44dd386f48c8df4e644d69381a9
ABZblack <109823782+ABZblack@users.noreply.github.com>
2024-02-29 11:00:18 -0600

    Update portabilidad.sql

--- MBB/portabilidad.sql
+++ MBB/portabilidad.sql
@@ -1,5 +1,4 @@
-SELECT *
+SELECT load_processing_timestamp, be_id, window_abd_date_fmt, operation, msisdn_ported, msisdn_backup, imsi, dida, dcr, rida, rcr, transaction_result, transaction_detail, comments
 FROM bss.portability_logs
-WHERE
-msisdn_ported IN (7226598206)or msisdn_backup in (7226598206)
+WHERE msisdn_ported IN (5518311460)or msisdn_backup in (5518311460)
 ORDER BY load_processing_timestamp desc;
