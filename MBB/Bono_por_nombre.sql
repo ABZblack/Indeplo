48562168e99a3df6842234db7dc53f2a6713c369
ABZblack <109823782+ABZblack@users.noreply.github.com>
2024-03-05 14:27:08 -0600

    Update Bono_por_nombre.sql

--- MBB/Bono_por_nombre.sql
+++ MBB/Bono_por_nombre.sql
@@ -1,3 +1,21 @@
+WITH catalogo
+as (
+    
+    SELECT nombre_promocion, descripcion_promocion, id_promocion, be_id, recurrencia, evento, offer_id_adquirido, offer_id_promocion, 
+    to_timestamp(cast(fecha_inicio_vigencia_promocion as STRING),'yyyyMMddHHmmss') fecha_inicio_vigencia_promocion,
+    to_timestamp(cast(fecha_fin_vigencia_promocion as STRING),'yyyyMMddHHmmss') fecha_fin_vigencia_promocion,
+    load_processing_timestamp, compra_y_elimina_oferta_actual
+    FROM service.catalogo_promociones
+),
+
+name
+as(
+    SELECT product_name, offering_id,be_name,be_id,index
+    FROM(Select product_name,be_name,be_id, offering_id, ROW_NUMBER() OVER (PARTITION BY offering_id ORDER BY end_date DESC) AS index
+            FROM billing.catalog_billing_product) as a
+    where index = 1
+)
+
 
 SELECT  nombre_promocion, 
         descripcion_promocion, 
